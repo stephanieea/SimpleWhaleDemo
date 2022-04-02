5ce12f7d6579f0be09ddd6cfcf6773fa06d3a5a0
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 17:51:11 -0600

    add sh

--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker pull ubuntu:latest
 docker run --name=my_container -v $(pwd):/home ubuntu echo "yo yo"
-docker exec my_container echo "yo" > log.txt 2> error-log.txt
+docker exec my_container sh -c "echo yo" > log.txt 2> error-log.txt
 tree -a
 echo "LOG"
 cat log.txt
