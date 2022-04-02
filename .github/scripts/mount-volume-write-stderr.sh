24d5762d1f13464c777ca0cecfa0236480bb0ccc
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 16:40:36 -0600

    mount from ubuntu

--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker image ls
-# docker run -v $(pwd):/home $DOCKER_IMAGE_TAG cat noexist.txt > log.txt 2> error-log.txt
+docker run -v $(pwd):/home ubuntu cat noexist.txt > log.txt 2> error-log.txt
