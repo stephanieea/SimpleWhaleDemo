e764e25558d109775246a8906d98c84fed48fda8
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 17:36:55 -0600

    add failing command

--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull ubuntu:latest
-docker run -v $(pwd):/home ubuntu echo "yo" > log.txt 2> error-log.txt
+docker run -v $(pwd):/home ubuntu cat doesnotexist.txt > log.txt 2> error-log.txt
 tree -a
 echo "LOG"
 cat log.txt
