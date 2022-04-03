66a4728d2d2a837af6c77e3fc218292931a6513a
stephanieea <stephanie.andrews@hearst.com>
2022-04-03 01:12:40 -0600

    intentional fail after proving intentional work will work as expected

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -3,7 +3,7 @@
 SITE_NAME=${1}
 SITE_PATH=${2}
 
-docker run -v $(pwd):/home --rm ubuntu echo "YO" > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
+docker run -v $(pwd):/home --rm ubuntu cat yo.txt > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
 
 if [ -s $SITE_NAME-error-log.txt ]; then
     echo -n $SITE_NAME, >> site-errors.txt
