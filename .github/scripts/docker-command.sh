d952381f124ebfafe0dbc95ec4afda2f02f59464
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 22:04:35 -0600

    keep it on the same line

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -6,6 +6,6 @@ SITE_PATH=${2}
 docker run -d -v $(pwd):/home --rm ubuntu cat not_a_file.txt > $SITE_NAME-log.txt > $SITE_NAME-error-log.txt
 
 if [ -s $SITE_NAME-error-log.txt ]; then
-    echo $SITE_NAME >> site-errors.txt
+    echo -n $SITE_NAME, >> site-errors.txt
 fi
 
