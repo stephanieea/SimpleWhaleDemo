b4980d3401eac41fb599d48d1babee1d277688cf
stephanieea <stephanie.andrews@hearst.com>
2022-04-03 00:42:55 -0600

    try with 2

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -3,7 +3,7 @@
 SITE_NAME=${1}
 SITE_PATH=${2}
 
-docker run -d -v $(pwd):/home --rm ubuntu echo "yo" > $SITE_NAME-log.txt > $SITE_NAME-error-log.txt
+docker run -d -v $(pwd):/home --rm ubuntu echo "yo" > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
 
 if [ -s $SITE_NAME-error-log.txt ]; then
     echo -n $SITE_NAME, >> site-errors.txt
