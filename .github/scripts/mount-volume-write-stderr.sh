54f69ecc9c8f5c7598e633c49abd04e69ad96acb
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 18:23:03 -0600

    try with bad cat

--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull ubuntu:latest
-docker run -d --name my_volume -v $(pwd):/home ubuntu echo "YO $NOT_A_VAR" > log.txt > error-log.txt
+docker run -d --name my_volume -v $(pwd):/home ubuntu cat not_a_file.txt > log.txt > error-log.txt
 tree -a
 echo "LOG"
 cat log.txt
