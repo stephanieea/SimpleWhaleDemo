84e92aa4df24e449dda8ec37969c2e852ea19614
stephanieea <stephanie.andrews@hearst.com>
2022-04-03 01:00:46 -0600

    update command

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -3,7 +3,7 @@
 SITE_NAME=${1}
 SITE_PATH=${2}
 
-docker run -d -v $(pwd):/home --rm ubuntu touch hi.txt
+docker run -d -v $(pwd):/home --rm ubuntu sh -c 'touch hi.txt'
 
 if [ -s $SITE_NAME-error-log.txt ]; then
     echo -n $SITE_NAME, >> site-errors.txt
