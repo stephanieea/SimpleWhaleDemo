579c35be4f8f103ae94aaeff8ff66059358dcb97
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 18:05:47 -0600

    use container id

--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 docker pull ubuntu:latest
-docker run --name my_container --rm ubuntu bash
-docker exec -d my_container touch hello.txt
+docker run --name my_volume -v $(pwd):/home --rm ubuntu bash
+CONTAINER_ID=$(docker ps -aqf "name=my_volume")
+docker exec -d $CONTAINER_ID touch hello.txt
 tree -a
 echo "LOG"
 cat log.txt
