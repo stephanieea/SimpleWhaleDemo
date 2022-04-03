54d10d24417410d5db60a5c60c52e18540b614fd
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 21:17:55 -0600

    try in jq parsing loop

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ANIMAL=${1}
+# ANIMAL=${1}
+SITE_NAME=${1}
+SITE_PATH=${2}
 
-docker run -d -v $(pwd):/home --rm ubuntu cat not_a_file.txt > $ANIMAL-log.txt > $ANIMAL-error-log.txt
+docker run -d -v $(pwd):/home --rm ubuntu cat not_a_file.txt > $SITE_NAME-log.txt > $SITE_NAME-error-log.txt
--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 docker pull ubuntu:latest
-animals=("capybara" "rhino" "emu")
-for animal in ${animals[@]}; do
-  echo $animal
-  .github/scripts/docker-command.sh $animal &
+# animals=("capybara" "rhino" "emu")
+# for animal in ${animals[@]}; do
+#   echo $animal
+#   .github/scripts/docker-command.sh $animal &
+# done
+# wait
+# tree -a
+# echo "CAPYBARA LOG"
+# cat capybara-log.txt
+# echo "CAPYBARA ERROR LOG"
+# cat capybara-error-log.txt
+
+# echo "rhino LOG"
+# cat rhino-log.txt
+# echo "rhino ERROR LOG"
+# cat rhino-error-log.txt
+
+# echo "emu LOG"
+# cat emu-log.txt
+# echo "emu ERROR LOG"
+# cat emu-error-log.txt
+
+for site in $(jq -rc 'to_entries[] | [.key, .value.path // "."]' manifest.json); do
+	SITE_NAME=$(echo $site | jq -r '.[0]')
+	SITE_PATH=$(echo $site | jq -r '.[1]')
+	# compile site assets in docker and run it in background
+	.github/scripts/docker-command.sh $SITE_NAME $SITE_PATH &
 done
 wait
 tree -a
-echo "CAPYBARA LOG"
-cat capybara-log.txt
-echo "CAPYBARA ERROR LOG"
-cat capybara-error-log.txt
+echo "fre LOG"
+cat fre-log.txt
+echo "fre ERROR LOG"
+cat fre-error-log.txt
 
-echo "rhino LOG"
-cat rhino-log.txt
-echo "rhino ERROR LOG"
-cat rhino-error-log.txt
+echo "bringatrailer LOG"
+cat bringatrailer-log.txt
+echo "bringatrailer ERROR LOG"
+cat bringatrailer-error-log.txt
 
-echo "emu LOG"
-cat emu-log.txt
-echo "emu ERROR LOG"
-cat emu-error-log.txt
+echo "caranddriver LOG"
+cat caranddriver-log.txt
+echo "caranddriver ERROR LOG"
+cat caranddriver-error-log.txt
