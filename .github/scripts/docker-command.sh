ff0897364967634134e2572453f65455bb03e4de
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 21:04:18 -0600

    change name and remove volume

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ANIMAL=${1}
 
-docker run -d --name my_volume -v $(pwd):/home ubuntu cat not_a_file.txt > $ANIMAL-log.txt > $ANIMAL-error-log.txt
+docker run -d --name this_volume -v $(pwd):/home --rm ubuntu cat not_a_file.txt > $ANIMAL-log.txt > $ANIMAL-error-log.txt
