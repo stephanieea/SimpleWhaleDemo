538e30215b18b3d40a936d805fe30d78306848c4
stephanieea <stephanie.andrews@hearst.com>
2022-04-02 21:25:50 -0600

    with the 2

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -3,4 +3,4 @@
 SITE_NAME=${1}
 SITE_PATH=${2}
 
-docker run -d -v $(pwd):/home --rm ubuntu cat not_a_file.txt > $SITE_NAME-log.txt > $SITE_NAME-error-log.txt
+docker run -d -v $(pwd):/home --rm ubuntu cat not_a_file.txt > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
