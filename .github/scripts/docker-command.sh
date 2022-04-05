d507b1d60a7b46d4e735e385b44770c7422f320f
stephanieea <stephanie.andrews@hearst.com>
2022-04-04 21:27:31 -0600

    ref correct var name

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ANIMAL=${1}
-SITE_NAME=${1}
+SCOPE_NAME=${1}
 SITE_PATH=${2}
 
 docker run --name $SITE_NAME -v $(pwd):/home --rm ubuntu cat doesnotexist.txt > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
