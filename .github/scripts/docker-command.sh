7c1770426a19653d585f4cf903331601069618cc
stephanieea <stephanie.andrews@hearst.com>
2022-04-04 21:18:15 -0600

    change build errors check and add rm back to docker command

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -5,14 +5,13 @@ SITE_PATH=${2}
 
 docker run --name $SITE_NAME -v $(pwd):/home --rm ubuntu cat doesnotexist.txt > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
 
-if [ -s $SITE_NAME-error-log.txt ]; then
-    echo "THERE IS AN ERROR LOG FOR $SITE_NAME"
-    echo $SITE_NAME, >> site-errors.txt
-fi
-
-# BUILD_ERRORS=$(grep -i such $SCOPE_NAME-error-log.txt) 
-# if [ ! -z $BUILD_ERRORS ]; then
-#     echo -n $SCOPE_NAME, >> site-errors.txt
+# if [ -s $SITE_NAME-error-log.txt ]; then
+#     echo "THERE IS AN ERROR LOG FOR $SITE_NAME"
+#     echo $SITE_NAME, >> site-errors.txt
 # fi
 
-docker rm $SITE_NAME
+BUILD_ERRORS=$(grep -i such $SCOPE_NAME-error-log.txt) 
+if [[ ! $BUILD_ERRORS ]]; then
+    echo -n $SCOPE_NAME, >> site-errors.txt
+fi
+
