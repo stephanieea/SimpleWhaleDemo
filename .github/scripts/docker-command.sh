b91f99be2794bf0f23e1139261bf5aeb607073aa
stephanieea <stephanie.andrews@hearst.com>
2022-04-04 20:31:01 -0600

    test failure and removing container after checking log

--- .github/scripts/docker-command.sh
+++ .github/scripts/docker-command.sh
@@ -3,10 +3,11 @@
 SITE_NAME=${1}
 SITE_PATH=${2}
 
-docker run -v $(pwd):/home --rm ubuntu echo "YO" > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
+docker run --name $SITE_NAME -v $(pwd):/home --rm ubuntu cat doesnotexist.txt > $SITE_NAME-log.txt 2> $SITE_NAME-error-log.txt
 
 if [ -s $SITE_NAME-error-log.txt ]; then
     echo "THERE IS AN ERROR LOG FOR $SITE_NAME"
     echo -n $SITE_NAME, >> site-errors.txt
 fi
 
+docker rm $SITE_NAME
