2181e505ec3e889f565c0d8b02f0dfdbc74028f7
stephanieea <stephanie.andrews@hearst.com>
2022-04-04 21:25:25 -0600

    try eof at eof

--- .github/scripts/mount-volume-write-stderr.sh
+++ .github/scripts/mount-volume-write-stderr.sh
@@ -41,9 +41,8 @@ if [ ! -s site-errors.txt ]; then
 	echo "HAS_BUILD_ERRORS=false" >> $GITHUB_ENV
 else
 	echo "HAS_BUILD_ERRORS=true" >> $GITHUB_ENV
-	echo 'BUILD_ERRORS<<EOF' >> $GITHUB_ENV
-	cat site-errors.txt >> $GITHUB_ENV
-	echo 'EOF' >> $GITHUB_ENV
 fi
-
+echo 'BUILD_ERRORS<<EOF' >> $GITHUB_ENV
+cat site-errors.txt >> $GITHUB_ENV
+echo 'EOF' >> $GITHUB_ENV
 
